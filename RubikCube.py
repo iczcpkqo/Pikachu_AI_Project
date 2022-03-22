798bb6190f8f8994d80c18973731371d78115f22
17205940 <50001907+Tamia99@users.noreply.github.com>
2022-03-22 15:38:08 +0000

    add get state functions, is terminal function

--- RubikCube.py
+++ RubikCube.py
@@ -1,21 +1,33 @@
 import numpy as np
+import copy as cp
 import CubeActions
 import random
 
 class RubikCube():
     def __init__(self):
         # 创建六个面，放在faces列表里，顺序为上（0），下（1），左（2），右（3），前（4），后（5）
-        self.faces = [np.zeros((3, 3))]
+        self.initial = [np.zeros((3, 3))]
         for i in range(1, 6):
-            self.faces.append(np.ones((3, 3)) + self.faces[i - 1])
+            self.initial.append(np.ones((3, 3)) + self.initial[i - 1])
+
+        self.startState = self.randomGenerate()
+        self.state = cp.deepcopy(self.startState)
+
+    def getInitialState(self):
+        return self.initial
 
     def randomGenerate(self):
+        startState = cp.deepcopy(self.initial)
         actions = self.getActions()
         for i in range(100):
-            self.faces = random.choice(actions)(self.faces)
+            startState = random.choice(actions)(startState)
+        return startState
 
-    def getCube(self):
-        return self.faces
+    def getStartState(self):
+        return self.startState
+
+    def getCurrentState(self):
+        return self.state
 
     def getActions(self):
         actions = [CubeActions.B, CubeActions.D, CubeActions.F,
@@ -25,21 +37,32 @@ class RubikCube():
                    ]
         return actions
 
-    def toString(self):
+    def move(self,action):
+        self.state = action(self.state)
+
+    def isTerminal(self,state):
+        return np.array_equal(state,self.initial)
+
+    def toString(self,state):
         print()
         for i in range(3):
-            print("     ", int(self.faces[0][i][0]), int(self.faces[0][i][1]), int(self.faces[0][i][2]))
+            print("     ", int(state[0][i][0]), int(state[0][i][1]), int(state[0][i][2]))
         for i in range(3):
-            print(int(self.faces[2][i][0]), int(self.faces[2][i][1]), int(self.faces[2][i][2]), end=" ")
-            print(int(self.faces[4][i][0]), int(self.faces[4][i][1]), int(self.faces[4][i][2]), end=" ")
-            print(int(self.faces[3][i][0]), int(self.faces[3][i][1]), int(self.faces[3][i][2]), end=" ")
-            print(int(self.faces[5][i][0]), int(self.faces[5][i][1]), int(self.faces[5][i][2]))
+            print(int(state[2][i][0]), int(state[2][i][1]), int(state[2][i][2]), end=" ")
+            print(int(state[4][i][0]), int(state[4][i][1]), int(state[4][i][2]), end=" ")
+            print(int(state[3][i][0]), int(state[3][i][1]), int(state[3][i][2]), end=" ")
+            print(int(state[5][i][0]), int(state[5][i][1]), int(state[5][i][2]))
         for i in range(3):
-            print("     ", int(self.faces[1][i][0]), int(self.faces[1][i][1]), int(self.faces[1][i][2]))
+            print("     ", int(state[1][i][0]), int(state[1][i][1]), int(state[1][i][2]))
         print()
 
 
 cube = RubikCube()
-cube.toString()
-cube.randomGenerate()
-cube.toString()
+cube.toString(cube.getInitialState())
+cube.toString(cube.getStartState())
+cube.toString(cube.getCurrentState())
+actions = cube.getActions()
+cube.move(actions[1])
+cube.toString(cube.getCurrentState())
+print(cube.isTerminal(cube.getCurrentState()))
+print(cube.isTerminal(cube.getInitialState()))
